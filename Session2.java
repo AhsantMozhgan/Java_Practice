d680073eaf5c06a21fef4254cd4278779725c425
Mozhgan Ahsant <mozhganahsant@gmail.com>
2024-07-29 12:34:32 -0700

    showMessageDialog

---
+++ Session2.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Session2 {
+    public static void main (String [] args) {
+        System.out.println("Hello Java!");
+        JOptionPane.showMessageDialog(null, "Hello :)");
+    }
+}
