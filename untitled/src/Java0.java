9c29f8c677e99ba78800ae833b04fe309e1e9976
Mozhgan Ahsant <mozhganahsant@gmail.com>
2024-07-29 12:07:51 -0700

    Add first commit

---
+++ untitled/src/Java0.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Java0 {
+    public static void main (String [] args) {
+        System.out.println("Hello Java!");
+        JOptionPane.showMessageDialog(null, "Hello :)");
+    }
+}
