abe19a1b1a4f3669e211be26e9a9eea1f30adf5d
Mozhgan Ahsant <mozhganahsant@gmail.com>
2024-07-29 19:49:49 -0700

    Compute CircleArea

---
+++ CircleArea.java
@@ -0,0 +1,9 @@
+public class CircleArea {
+    public static void main (String [] srgs) {
+        int r = 2;
+        final double PI = 3.14;
+        double area = PI * r * r;
+        double p = 2 * PI * r;
+        System.out.println(p);
+    }
+}
