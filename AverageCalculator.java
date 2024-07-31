725267943e82fbc21c1d41b174424c262104d7d3
Mozhgan Ahsant <mozhganahsant@gmail.com>
2024-07-31 12:09:01 -0700

    AverageCalculator

---
+++ AverageCalculator.java
@@ -0,0 +1,24 @@
+//Write a Java program calculates the average of a series of numbers entered by the user.
+//The input ends when the user enters the number 0
+
+import java.util.Scanner;
+
+public class AverageCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter some numbers and press enter: ");
+
+        int data;
+        data = input.nextInt();
+        int sum = 0;
+        int count = 0;
+        double avg;
+        while (data !=0)  {
+            sum += data;
+            count++;
+            data = input.nextInt();
+        }
+        avg = (double)sum / count;
+        System.out.println("Average: " + avg);
+    }
+}
--- Factorials.java
+++ Factorials.java
@@ -1,13 +1,27 @@
+import javax.print.attribute.standard.OrientationRequested;
 import java.util.Scanner;
 
 public class Factorials {
     public static void main(String[] args) {
         int n;
         Scanner input = new Scanner (System.in);
+        System.out.print("Enter a number: ");
         n = input.nextInt ();
         int f = 1;
-        for (int i = 2; i <= n; i++)
+        for (int i = 2; i <= n; i++) {
             f = f * i;
+
+//        OR using while loop
+//        int i = 2;
+//        while (i <= n) {
+//            f = f * i;
+//            ++i;
+//        }
+            System.out.println(i);
+
+        }
         System.out.println (n + "! = " + f);
+
+
     }
 }
