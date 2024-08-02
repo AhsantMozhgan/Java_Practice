fa53479f5e8de60de6b7396713f843e01eb0ea08
Mozhgan Ahsant <mozhganahsant@gmail.com>
2024-08-02 13:52:50 -0700

    MultiplicationTable formatted to align correctly

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -3,18 +3,21 @@
 public class MultiplicationTable {
     public static void main(String[] args) {
         System.out.println("\t\t\t\tMultiplication Table ");
-//        System.out.println("\t");
+//        System.out.print("\t");
 //        for (int i = 1; i <= 9;i++)
-//        System.out.print( "\t" +  i );
-        System.out.println("          1  2  3  4  5  6  7  8  9   ");
+//        System.out.print("\t" + i);
+//        System.out.println();
+        System.out.println("           1   2   3   4   5   6   7   8   9   ");
         for (int i = 0; i < 50; i++)
             System.out.print("-");
         System.out.println();
 
         for (int row = 1; row <= 9; row++) {
-            System.out.print(row+ "   |   ");
+            System.out.print(row + "   |   ");
             for (int column = 1; column <= 9; column++) {
-                System.out.print("  " + row * column);
+//                System.out.print("  " + row * column);
+                // Print the product of the current row and column, formatted to align correctly
+                System.out.printf("%4d", row * column);
             }
             System.out.println();
         }
