bc7709764281d4d85b86b17e71c69161a98e1546
Mozhgan Ahsant <mozhganahsant@gmail.com>
2024-08-03 19:36:25 -0700

    StarTriangle

--- StarTriangle.java
+++ StarTriangle.java
@@ -1,21 +1,18 @@
-//a simple Java program that prompts the user to enter a number and then prints a square pattern of stars (*),
-// where the number of rows and columns is equal to the entered number.
+//write a Java program that prompts the user to enter a number n and then prints a
+// right-angled triangle pattern of stars (*), where the number of rows is equal to
+// the entered number, and the number of stars in each row starts from 1 and increases up to n.
 import java.util.Scanner;
 
-public class StarSquare {
+public class StarTriangle {
     public static void main(String[] args) {
         int number;
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a number: ");
         number = input.nextInt();
         for (int row = 1; row <= number; row++) {
-            for (int column = 1; column <= number; column++)
+            for (int column = 1; column <= row; column++)
                 System.out.print("*  ");
             System.out.println();
         }
     }
 }
-
-
-
-
