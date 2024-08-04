25cac93b3806e28a2786152f686d749d7321eef7
Mozhgan Ahsant <mozhganahsant@gmail.com>
2024-08-03 17:47:44 -0700

    DisplayingPrimeNumbers-10 numbers

---
+++ DisplayingPrimeNumbers.java
@@ -0,0 +1,23 @@
+//Write a program that displays the first, 50 prime numbers
+// in five lines, each of which contains ten numbers.
+
+public class DisplayingPrimeNumbers {
+    public static void main(String[] args) {
+        int number = 2;
+        int count = 0;
+        while(count < 10) {
+            if (number % 2!= 0) {
+                boolean isPrime = true;
+                for (int i = 3; i * i <= number && isPrime; i+=2)
+                    if (number % i == 0)
+                        isPrime = false;
+
+                if (isPrime) {
+                    System.out.print(number + "   ");
+                    count++;
+                }
+            }
+            number++;
+        }
+    }
+}
---
+++ MyPractice.java
@@ -0,0 +1,4 @@
+public class MyPractice {
+    public static void main(String[] args) {
+    }
+}
