96ed5f57e760cf7532bb4cd979130424913c1c3e
Mozhgan Ahsant <mozhganahsant@gmail.com>
2024-08-28 18:37:13 -0700

    error for creating more than one constructor

--- CircleClass.java
+++ CircleClass.java
@@ -1,6 +1,7 @@
 public class CircleClass {
     int radius;
-    CircleClass() {     // default constructor
+    // default constructor
+    CircleClass() {     // incorrect form: 'void CircleClass' because constructor does not return anything
         radius = 1;     // data type is declared in the CircleClass and no need to declare here
     }
 }
--- MainClass.java
+++ MainClass.java
@@ -2,5 +2,8 @@ public class MainClass {
     public static void main(String[] args) {
         CircleClass c1 = new CircleClass();
         System.out.println(c1.radius);
+
+        // this is incorrect because we can not call a constructor more than once
+//        c1.CircleClass();   // Error
     }
 }
