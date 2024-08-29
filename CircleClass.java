5e7a9799e7324f5f5800964b494fcd106e3b746c
Mozhgan Ahsant <mozhganahsant@gmail.com>
2024-08-28 18:18:43 -0700

    change default constructor

---
+++ CircleClass.java
@@ -0,0 +1,6 @@
+public class CircleClass {
+    int radius;
+    CircleClass() {     // default constructor
+        radius = 1;     // data type is declared in the CircleClass and no need to declare here
+    }
+}
---
+++ MainClass.java
@@ -0,0 +1,6 @@
+public class MainClass {
+    public static void main(String[] args) {
+        CircleClass c1 = new CircleClass();
+        System.out.println(c1.radius);
+    }
+}
