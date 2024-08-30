fda9733982880232ad7b0591f020d0e540abb24d
Mozhgan Ahsant <mozhganahsant@gmail.com>
2024-08-30 15:04:00 -0700

    make an attribute private and write getter method called 'getRadius()' to return value of radius

--- CircleClass.java
+++ CircleClass.java
@@ -1,5 +1,5 @@
 public class CircleClass {
-    int radius;
+    private int radius;
     // default constructor
     CircleClass() {     // incorrect form: 'void CircleClass' because constructor does not return anything
 //        radius = 1;     // data type is declared in the CircleClass and no need to declare here
--- MainClass.java
+++ MainClass.java
@@ -1,16 +1,17 @@
 public class MainClass {
     public static void main(String[] args) {
         CircleClass c1 = new CircleClass();
-        System.out.println(c1.radius);
+//        System.out.println(c1.radius);
 
         // this is incorrect because we can not call a constructor more than once
 //        c1.CircleClass();   // Error
 
         // printing constructor with one parameter
         CircleClass c2 = new CircleClass(3);
-        System.out.println(c2.radius);
+//        System.out.println(c2.radius);
 
-        // We just can see the value of radius and we can not change its value
+        // We just can see the value of an attribute like radius and we can not change its value
+        // we used 'private' for radius in the CircleClass and write getRadius method to return the value of radius
         System.out.println("Read radius: " + c1.getRadius());
     }
 }
