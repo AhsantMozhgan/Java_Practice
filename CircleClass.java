68a6b6719d97ffbacf2b5bd20047ca3d3c7b39a0
Mozhgan Ahsant <mozhganahsant@gmail.com>
2024-09-07 13:43:50 -0700

    Add modifier

--- CircleClass.java
+++ CircleClass.java
@@ -1,18 +1,18 @@
 public class CircleClass {
     private int radius;
     // default constructor
-    CircleClass() {     // incorrect form: 'void CircleClass' because constructor does not return anything
+    public CircleClass() {     // incorrect form: 'void CircleClass' because constructor does not return anything
 //        radius = 1;     // data type is declared in the CircleClass and no need to declare here
     this(1);    // calling another constructor inside this constructor
     }
 
     // constructor with one parameter
-    CircleClass(int r) {
+    public CircleClass(int r) {
         radius = r;
     }
 
     // OR
-//    CircleClass(int r) {
+//   public CircleClass(int r) {
 //        if (r > 0) {
 //            radius = r;
 //        } else {
@@ -21,15 +21,17 @@ public class CircleClass {
 //    }
 
     // Using getter if we want to know the radius (return the value of radius)
-    int getRadius() {
+    public int getRadius() {
         return radius;
     }
 
-    void setRadius(int r) {
+    public void setRadius(int r) {
         if (r > 0) {
             radius = r;
         }
     }
 
-
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
 }
