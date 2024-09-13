7f343512a9058be845d478537b319e459895433d
Mozhgan Ahsant <mozhganahsant@gmail.com>
2024-09-13 14:03:08 -0700

    modified access modifiers

--- MainClass.java
+++ MainClass.java
@@ -1,4 +1,5 @@
-import testPackage.A;
+//import testPackage.A;
+import testPackage.*;
 public class MainClass {
 //    public static void main(String[] args) throws InterruptedException {
 //        CircleClass c1 = new CircleClass();
@@ -67,6 +68,16 @@ public class MainClass {
         change(a, test);
          System.out.println(a);         // output: 1
          System.out.println(test.a);    // output: 3
+
+
+         // test private, public, protected and default access
+//    A a = new A();      // error because a is private in class A
+
+         // to have no error we need to write 'import testPackage.A;' or 'import testPackage.*; at top of the code'
+     A a1 = new A();
+//     a1.a = 2;   // error (private access)
+     a1.b = 2;    // public access
+//     a1.c = 2;     // error (default access)
     }
 
     public static void change(int a, Test b) { // a is a number and b is an object from Test class
@@ -79,14 +90,4 @@ public class MainClass {
         // 'b' is reference type (if we change b's attribute, the main object from Test class will change)
         b.a = 3;    // 'b' is an object from Test class and Test class has an attribute called 'a'.
     }
-
-    // test private, public, protected and default access
-//    A a = new A();      // error because a is private in class A
-
-    // to have no error we need to write 'import testPackage.A;' or 'import testPackage.*; at top of the code'
-    A a1 = new A();
-//     a1.a = 2;   // error (private access)
-//     a1.b = 2;    // public access
-//     a1.c = 2;     // error (default access)
-
 }
--- testPackage/B.java
+++ testPackage/B.java
@@ -3,7 +3,7 @@ package testPackage;
 public class B {
     public static void main(String[] args) {
         A a2 = new A();
-        a2.a = 2;   // error (private access)
+//        a2.a = 2;   // error (private access)
         a2.b = 2;    // public access
         a2.c = 2;     // default access
     }
