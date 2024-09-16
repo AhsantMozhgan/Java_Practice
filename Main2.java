fe1245376dbfdcbdbed473f971aedce31baa49dc
Mozhgan Ahsant <mozhganahsant@gmail.com>
2024-09-16 13:57:19 -0700

    compare two strings

--- Main2.java
+++ Main2.java
@@ -13,9 +13,19 @@ public class Main2 {
 //        String name = input.next(); // For example, write: 'How are you', and output is: 'How'
 //        System.out.println(name);
 
-        // Example for 'nextLine()'
-        System.out.println("Write something for nextLine(): ");
-        String name2 = input.nextLine(); // For example, write: 'How are you', and output is: 'How are you'
-        System.out.println(name2);
+//        // Example for 'nextLine()'
+//        System.out.println("Write something for nextLine(): ");
+//        String name2 = input.nextLine(); // For example, write: 'How are you', and output is: 'How are you'
+//        System.out.println(name2);
+
+//        if you call any method in a string, string's content will not change
+
+        String nameOne = "Ali Reza";
+        String nameTwo = new String("Ali Reza");
+//        if (nameOne == nameTwo) {      // this is not a correct way and output is nothing
+        if (nameOne.equals(nameTwo)) {   // this is a correct way and output would be 'he strings are equal'
+            System.out.println("The strings are equal");
+        }
+
     }
 }
