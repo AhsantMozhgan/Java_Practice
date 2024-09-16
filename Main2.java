287643fb5e8ab7fcad7d8f3a45465e0bb43a8804
Mozhgan Ahsant <mozhganahsant@gmail.com>
2024-09-16 11:32:14 -0700

    example for nextLine()

--- Main2.java
+++ Main2.java
@@ -8,9 +8,14 @@ public class Main2 {
 //        name = "Ali";         // refer to 'Ali' again
 
         Scanner input = new Scanner(System.in);
-        // Example for 'next()'
-        System.out.println("Write something: ");
-        String name = input.next(); // For example, write: 'How are you', and output is: 'How'
-        System.out.println(name);
+//        // Example for 'next()'
+//        System.out.println("Write something: ");
+//        String name = input.next(); // For example, write: 'How are you', and output is: 'How'
+//        System.out.println(name);
+
+        // Example for 'nextLine()'
+        System.out.println("Write something for nextLine(): ");
+        String name2 = input.nextLine(); // For example, write: 'How are you', and output is: 'How are you'
+        System.out.println(name2);
     }
 }
