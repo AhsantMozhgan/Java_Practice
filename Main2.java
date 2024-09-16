edfb0d992758f147155129c64891d40da2696961
Mozhgan Ahsant <mozhganahsant@gmail.com>
2024-09-16 14:01:48 -0700

    compare with ignore case

--- Main2.java
+++ Main2.java
@@ -20,12 +20,21 @@ public class Main2 {
 
 //        if you call any method in a string, string's content will not change
 
-        String nameOne = "Ali Reza";
-        String nameTwo = new String("Ali Reza");
+//        String nameOne = "Ali Reza";
+//        String nameTwo = new String("Ali Reza");
 //        if (nameOne == nameTwo) {      // this is not a correct way and output is nothing
-        if (nameOne.equals(nameTwo)) {   // this is a correct way and output would be 'he strings are equal'
-            System.out.println("The strings are equal");
-        }
+//        if (nameOne.equals(nameTwo))   // this is a correct way and output would be 'he strings are equal'
+//            System.out.println("The strings are equal");
+//            else
+//            System.out.println("The strings are not equal");
 
+
+        // compare with ignore case
+        String nameOne = "ali Reza";
+        String nameTwo = new String("Ali Reza");
+        if (nameOne.equalsIgnoreCase(nameTwo))   // this is a correct way and output would be 'he strings are equal'
+            System.out.println("The strings are equal");
+        else
+            System.out.println("The strings are not equal");
     }
 }
