f23d5828aec6bcecf4f3203d70747e3c94d9a3e2
Mozhgan Ahsant <mozhganahsant@gmail.com>
2024-09-18 10:58:29 -0700

    ArrayList examples

---
+++ ArrayListTest.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ArrayListTest {
+    public static void main(String[] args) {
+//        ArrayList names = new ArrayList(3); // if we have more than 3 elements the ArrayList capacity will double
+//        ArrayList<String> names = new ArrayList<String>(3); // we need to declare data type of the names
+
+        // OR
+//        size of  this Array list is 0 and its capacity is 3
+        ArrayList<String> names = new ArrayList(3); // we can omit the second <String>
+
+        // to add an element to ArrayList we have two methods: add(object element) and add(int index, object element)
+        // add(object element) will add element to the last
+        // add(int index, object element) will add element to a specified index that we want
+        System.out.println(names);      // []
+//        names.add(12); // error because the data type is declared as String
+        names.add("Ali");
+        names.add("Naghi");
+        names.add("Abbas");
+        names.add("Taghi");
+        System.out.println(names);  // names will add from end of the ArrayList in order        // [Ali, Naghi, Abbas, Taghi]
+        Collections.sort(names);    // must import Collections class
+        System.out.println(names);
+
+        
+    }
+}
