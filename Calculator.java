19666b53ece2aaf5a042b0a4e3141f091d2f36d1
Mozhgan Ahsant <mozhganahsant@gmail.com>
2024-09-30 15:17:30 -0700

    Creating First Java Swing GUI Application with IntelliJ IDEA IDE (youtube video)

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,4 @@
 public class Calculator extends javax.swing.JFrame {
-
+    // Creating First Java Swing GUI Application with IntelliJ IDEA IDE
+    // https://www.youtube.com/watch?v=5vSyylPPEko&t=1s
 }
