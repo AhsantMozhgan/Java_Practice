f128099a921a2252c52df3f8d66f063e1310d894
Mozhgan Ahsant <mozhganahsant@gmail.com>
2024-09-30 13:58:05 -0700

    using JTextField from NewJFrame.form

--- NewJFrame.java
+++ NewJFrame.java
@@ -2,4 +2,7 @@ import javax.swing.*;
 
 public class NewJFrame {
     private JPanel panel1;
+    private JTextField textField1;
+    private JTextField textField3;
+    private JTextField textField2;
 }
