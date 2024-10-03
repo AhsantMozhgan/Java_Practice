0859ba1c94abfef5de852ca71d90a14156beb4a1
Mozhgan Ahsant <mozhganahsant@gmail.com>
2024-10-03 13:31:51 -0700

    getAbsoluteFile will return the address of file

--- FileTest.java
+++ FileTest.java
@@ -5,5 +5,7 @@ public class FileTest {
         File file = new File("1.txt");
         System.out.println(file.exists());      // true
         System.out.println(file.isDirectory());      // false
+
+        System.out.println(file.getAbsoluteFile());     // this method returns the Address of file
     }
 }
