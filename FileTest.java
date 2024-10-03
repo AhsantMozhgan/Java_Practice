ac0281e27e27d061fc2127064cc7d92fb5ff2153
Mozhgan Ahsant <mozhganahsant@gmail.com>
2024-10-03 13:59:35 -0700

    representing a directory's contents (folder's contents)

--- FileTest.java
+++ FileTest.java
@@ -10,5 +10,12 @@ public class FileTest {
         System.out.println(file.getName());     // 1.txt
         System.out.println(file.getParent());   // null
         System.out.println(file.length());      // 15       // each character is 1 byte, also each character in Farsi is 2 byte
+
+        File file1 = new File("testPackage");   // we put directory (folder name) here
+        String [] names = file1.list();
+        for (String name: names) {
+            // Returns an array of Strings representing a directory's contents (folder's contents). Returns null if the File object does not represent a directory.
+            System.out.println(name);
+        }
     }
 }
