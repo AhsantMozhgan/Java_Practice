2929aea7ccedf87507f151d3c198bdc596efe0ee
Mozhgan Ahsant <mozhganahsant@gmail.com>
2024-10-07 13:28:06 -0700

    // we want to read an image

--- FileTest.java
+++ FileTest.java
@@ -111,27 +111,34 @@ public class FileTest {
         // for reading from file
 
 //        We want to create a line in the memory from the information that is in our memory and put it in the list array.
-        try {
-            //        File f = new File("1.txt");
-            Scanner reader1 = new Scanner(new File("scores.txt"));
-//            System.out.printf("%15s%15s%15s", "first name", "last name", "score");
-//            System.out.println();
-//            System.out.println("====================================================");
-            ArrayList<Student> students = new ArrayList();
-            while (reader1.hasNext()) {
-                String firstName = reader1.next();
-                String lastName = reader1.next();
-                int score = reader1.nextInt();
-//                System.out.printf("%15s%15s%15s", firstName, lastName, score);
-//                System.out.println();
-//                String studentInfo = reader1.nextLine();
-//                System.out.println(studentInfo);
-                Student student = new Student(firstName, lastName, score);
-                students.add(student);
-            }
-        } catch (FileNotFoundException ex)  {
-            System.out.println("File doesn't exist");
+//        try {
+//            //        File f = new File("1.txt");
+//            Scanner reader1 = new Scanner(new File("scores.txt"));
+////            System.out.printf("%15s%15s%15s", "first name", "last name", "score");
+////            System.out.println();
+////            System.out.println("====================================================");
+//            ArrayList<Student> students = new ArrayList();
+//            while (reader1.hasNext()) {
+//                String firstName = reader1.next();
+//                String lastName = reader1.next();
+//                int score = reader1.nextInt();
+////                System.out.printf("%15s%15s%15s", firstName, lastName, score);
+////                System.out.println();
+////                String studentInfo = reader1.nextLine();
+////                System.out.println(studentInfo);
+//                Student student = new Student(firstName, lastName, score);
+//                students.add(student);
+//            }
+//        } catch (FileNotFoundException ex)  {
+//            System.out.println("File doesn't exist");
+//
+//        }
 
+        // we want to read an image
+        try {
+            InputStream imgReader = new FileInputStream("1.png");
+        } catch (FileNotFoundException ex) {
+            System.out.println("Input file doesn't exist");
         }
 
     }
