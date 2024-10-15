1bfc227979930712d9ca83c1b6e030a202867f9b
Mozhgan Ahsant <mozhganahsant@gmail.com>
2024-10-15 14:29:41 -0700

    create Stack class

---
+++ DataStructure/Stack.java
@@ -0,0 +1,7 @@
+package DataStructure;
+
+public class Stack {
+    // the length of this array is zero here because there's no space for this array in the memory.
+    // when we make new object we get space for this array in the memory.
+    private int s[];
+}
