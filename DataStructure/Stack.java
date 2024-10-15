ce93072c0954a20d15ec5435508bf9d5075473ce
Mozhgan Ahsant <mozhganahsant@gmail.com>
2024-10-15 14:42:31 -0700

    create default constructor and create new object into constructor

--- DataStructure/Stack.java
+++ DataStructure/Stack.java
@@ -1,15 +1,18 @@
 package DataStructure;
 
-//public class Stack {
-//    // the length of this array is zero here because there's no space for this array in the memory.
-//    // when we make new object we get space for this array in the memory.
-//    private int s[];
-//}
+public class Stack {
+    // the length of this array is zero here because there's no space for this array in the memory.
+    // when we make new object we get space for this array in the memory.
+    private int s[];
+    public Stack() {
+        s = new int [10];
+    }
+}
 
 // Generic definition
 // if we want to do not define the type of the element, we use generic definition.
 // for example <T> here is something Undefined which is not 'int' or it is not 'String' or anything else
 // when the type of <T> will be defined? when we want to create new object from Stack<T> class
-public class Stack<T> {
-    private T s[];
-}
+//public class Stack<T> {
+//    private T s[];
+//}
