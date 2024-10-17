926fbbc081adb7ed9378868914322178a955561c
Mozhgan Ahsant <mozhganahsant@gmail.com>
2024-10-17 13:41:23 -0700

    create new object from Stack class into the Test class

--- DataStructure/Test.java
+++ DataStructure/Test.java
@@ -1,15 +1,22 @@
 package DataStructure;
 
 public class Test {
-    static long fact(int n) {
-        if (n == 0)
-            return 1;
-
-        return n * fact(n - 1);
-    }
+//    static long fact(int n) {
+//        if (n == 0)
+//            return 1;
+//
+//        return n * fact(n - 1);
+//    }
+//
+//    public static void main(String[] args) {
+//       // fact(3);
+//        System.out.println(fact(3));
+//    }
 
     public static void main(String[] args) {
-       // fact(3);
-        System.out.println(fact(3));
+        Stack stack = new Stack(2);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);      // error because we can not push more than 2 items in here (Stack stack = new Stack(2);)
     }
 }
