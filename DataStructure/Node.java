109562e2f9a981c9fe613f3f86c0a2f677885423
Mozhgan Ahsant <mozhganahsant@gmail.com>
2024-10-21 18:22:25 -0700

    create Node class for linkedList

---
+++ DataStructure/Node.java
@@ -0,0 +1,16 @@
+package DataStructure;
+
+public class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+    }
+
+    public static void main(String[] args) {
+        Node a  = new Node(2);
+        Node b  = new Node(5);
+        Node c  = new Node(7);
+    }
+}
