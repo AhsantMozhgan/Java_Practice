9d240843316237649ef5ad8f9349149f8bab90c8
Mozhgan Ahsant <mozhganahsant@gmail.com>
2024-10-22 10:44:00 -0700

    make addLast(int element) method

--- DataStructure/LinkedList.java
+++ DataStructure/LinkedList.java
@@ -10,8 +10,28 @@ public class LinkedList {
     public void addFirst(int element) {
         // we do not use exception here because we do not have problem for being linedList full
         // but in the array we used exception because we had problem for being Array full
+        // addFirst has O(1)
         Node newNode = new Node(element);
         newNode.next = first;
+
+        first = newNode;
+
+        size++;
+    }
+
+    public void addLast(int element) {
+        Node newNode = new Node(element);
+
+        // last node points to the first node
+        Node last = first;
+
+        while (last.next != null) {
+            // To refer one node to another node we write: oneNode = anotherNode.next;
+            last = last.next;
+        }
+        last.next = newNode;
+
+        size++;
     }
 
 }
