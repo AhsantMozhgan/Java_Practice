8ec8ae67e74612cd723f3d8cb889f7c9da02f263
Mozhgan Ahsant <mozhganahsant@gmail.com>
2024-10-22 09:33:57 -0700

    create LinkedList class

---
+++ DataStructure/LinkedList.java
@@ -0,0 +1,11 @@
+package DataStructure;
+
+public class LinkedList {
+    private Node first;
+    private int size;
+    // we do not need to write constructor because 'first' node does not refer to any node and 'size' has zero value
+    // we do not write constructor here because 'first' and 'size' have their default value
+    // we use constructor if we want to use any value except default value
+
+
+}
