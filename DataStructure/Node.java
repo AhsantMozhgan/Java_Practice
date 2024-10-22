3a937c1a395ea3aef3a903221816d8730d70d4e1
Mozhgan Ahsant <mozhganahsant@gmail.com>
2024-10-22 09:19:36 -0700

    using for-loop to connect nodes

--- DataStructure/Node.java
+++ DataStructure/Node.java
@@ -16,15 +16,25 @@ public class Node {
 //        a.next = b;
 //        b.next = c;
 
-        // another way to connect these three nodes is:
-        Node first  = new Node(2);
+//        // another way to connect these three nodes is:
+//        Node first  = new Node(2);
+//        Node last = first;
+//
+//        last.next = new Node(5);
+//        last = last.next;
+//
+//        last.next = new Node(7);
+//        last = last.next;
+
+        // using for-loop to connect nodes
+        // we put first node outside of loop
+        Node first = new Node(7);
         Node last = first;
 
-        last.next = new Node(5);
-        last = last.next;
-
-        last.next = new Node(7);
-        last = last.next;
+        for (int i = 14; i <= 100; i+=7) {
+            last.next = new Node(i);
+            last = last.next;
+        }
 
         // using current node to check do we reached to the end of the linked-list or not
         Node current = first;
