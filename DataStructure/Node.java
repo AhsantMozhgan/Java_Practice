5fb14dc391152b20c757c6caa39f0cff749d4ecb
Mozhgan Ahsant <mozhganahsant@gmail.com>
2024-10-22 09:06:49 -0700

    using first and last to connect nodes

--- DataStructure/Node.java
+++ DataStructure/Node.java
@@ -9,11 +9,23 @@ public class Node {
     }
 
     public static void main(String[] args) {
-        Node a  = new Node(2);
-        Node b  = new Node(5);
-        Node c  = new Node(7);
+//        Node a  = new Node(2);
+//        Node b  = new Node(5);
+//        Node c  = new Node(7);
+//
+//        a.next = b;
+//        b.next = c;
 
-        a.next = b;
-        b.next = c;
+        // another way to connect these three nodes is:
+        Node first  = new Node(2);
+        Node last = first;
+
+        last.next = new Node(5);
+        last = last.next;
+
+        last.next = new Node(7);
+        last = last.next;
+
+
     }
 }
