cd6a0d7711ba6f7f29794ea12d8067924c99170b
Mozhgan Ahsant <mozhganahsant@gmail.com>
2024-10-22 10:24:04 -0700

    make addFirst(int element) method

--- DataStructure/LinkedList.java
+++ DataStructure/LinkedList.java
@@ -7,5 +7,11 @@ public class LinkedList {
     // we do not write constructor here because 'first' and 'size' have their default value
     // we use constructor if we want to use any value except default value
 
+    public void addFirst(int element) {
+        // we do not use exception here because we do not have problem for being linedList full
+        // but in the array we used exception because we had problem for being Array full
+        Node newNode = new Node(element);
+        newNode.next = first;
+    }
 
 }
