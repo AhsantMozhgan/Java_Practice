ebcaeafe9c465e41c115349fd3b3cd40e56ae805
Mozhgan Ahsant <mozhganahsant@gmail.com>
2024-11-02 18:40:05 -0700

    inorder Navigation

--- DataStructure/BinaryTree.java
+++ DataStructure/BinaryTree.java
@@ -1,4 +1,19 @@
 package DataStructure;
 
 public class BinaryTree {
+    TreeNode root;
+
+    public void inorderPrint() {
+        inorderPrint(root);
+    }
+
+    public void inorderPrint(TreeNode r) {
+        if (r == null) {
+            return;
+        }
+
+        inorderPrint(r.leftChild);
+        System.out.println(" " + r.data );
+        inorderPrint(r.rightChild);
+    }
 }
