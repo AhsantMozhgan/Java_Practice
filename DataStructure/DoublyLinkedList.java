d1a734b51995c79303de2e3250e8bc7be3fd2347
Mozhgan Ahsant <mozhganahsant@gmail.com>
2025-01-08 18:39:40 -0800

    creat DoublyLinkedList, addFirst method

---
+++ DataStructure/DoublyLinkedList.java
@@ -0,0 +1,68 @@
+package DataStructure;
+
+public class DoublyLinkedList {
+    private Node first;
+    private Node last;
+    private int size;
+
+    public DoublyLinkedList() {
+        this.size = 0;
+        this.first = null;
+        this.last = null;
+    }
+
+    public class Node {
+        private int data;
+        private Node next;
+        private Node prev;
+
+        public Node (int data) {
+            this.data = data;
+            this.prev = null;
+            this.next = null;
+        }
+
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void addFirst(int element) {
+        Node newNode = new Node(element);
+        if(isEmpty()) {
+            // The list was previously empty.
+            //A new node (newNode) has just been created.
+            //This new node becomes both the first and last element of the list.
+            first = last = newNode;
+        } else {
+            newNode.next = first;
+            first.prev = newNode;
+            first = newNode;
+        }
+        size++;
+
+    }
+
+    public void print() {
+        System.out.print("[");
+        Node current = first;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.print("]");
+    }
+
+
+    public static void main(String[] args) {
+        DoublyLinkedList list = new DoublyLinkedList();
+        System.out.println("Adding elements:");
+        for (int i = 30; i >= 5; i-=5) {
+            list.addFirst(i);
+        }
+        list.print();
+
+    }
+
+}
